3446c825c0076e56f0965893a1b8d53792198164
Saransh Singh <singh.saransh90@gmail.com>
2021-12-19 22:03:54 +0530

    landing_page: Add landing page to request URLs.
    * Add a basic route with a HTML rendered page where
      a user can request for a short URL or their original
      URL back.
    
    * Create a basic HTML page for the above route.
    
    * Write tests for the above route.
    
    * Add IDE files to .gitignore.

--- api/main.py
+++ api/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+"""
+File contains all the API and HTML page related code.
+"""
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 # Instantiate an instance of FastAPI.
 app = FastAPI()
+
+# Create the object that can be re-used later by
+# multiple routes.
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
@@ -15,3 +24,22 @@ async def home():
 
     return {"message": "Hello World"}
 
+
+@app.get("/url-coder", response_class=HTMLResponse)
+async def url_coder(request: Request):
+    """
+    This route provides the page where a user can input
+    his desired URL and request a shorter version of
+    the URL or request the original URL back from the
+    shorter version.
+    Args:
+        request (Request): Data sent from the client side.
+            Example - a browser sending data to the backend.
+    Returns:
+        TemplateResponse: Rendering the HTML page as provided
+            by the 'name' parameter of TemplateResponse.
+    """
+
+    return templates.TemplateResponse(
+        name="landing_page.html", context={"request": request}
+    )
--- tests.py
+++ tests.py
@@ -1,3 +1,7 @@
+"""
+File contains all the test suites for the main.py file.
+"""
+
 from fastapi.testclient import TestClient
 from api.main import app
 
@@ -5,8 +9,23 @@ client = TestClient(app)
 
 
 def test_home():
+    """
+    Tests for the ``home`` route in ``main.py``.
+    """
+
     response = client.get("/")
 
     assert response.status_code == 200
     assert response.json() == {"message": "Hello Finn"}
 
+
+def test_url_coder():
+    """
+    Tests for the ``url_coder`` route in ``main.py``.
+    """
+
+    response = client.get("/url-coder")
+
+    assert response.status_code == 200
+    assert response.template.name == "landing_page.html"  # type: ignore
+    assert "request" in response.context  # type: ignore
